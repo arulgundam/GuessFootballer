dde87a11d441579f9c4cd71b8959d03364a39ff9
arulgundam <arul.gundam@gmail.com>
2023-06-26 16:01:15 -0400

    tailwind, split html+css, wrong guess gone (fix)

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./src/timeless_mode.html", "./src/about.html", "./src/contact.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
