a05cef8b2d498f80e954e097f8dc9184a742627e
arulgundam <arul.gundam@gmail.com>
2024-02-27 01:09:42 -0500

    data accuracy date added

--- src/about.jsx
+++ src/about.jsx
@@ -2,6 +2,7 @@ const About = () => (
   <>
     <div>
       <h1>About</h1>
+      <p>Data accurate as of June 20, 2023.</p>
       <a href="https://github.com/arulgundam/GuessFootballer">Source Code</a>
       <p></p>
       <a href="https://www.linkedin.com/in/arul-gundam/">LinkedIn</a>
