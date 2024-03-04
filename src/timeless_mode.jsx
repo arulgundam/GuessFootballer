3161af8c1ee35159665620590ed0d2a42b61574e
arulgundam <arul.gundam@gmail.com>
2024-03-04 17:52:18 -0500

    .

--- src/timeless_mode.jsx
+++ src/timeless_mode.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState, useRef, useReducer } from "preact/hooks";
 import tw from 'tailwind-styled-components';
 import Confetti from "react-confetti";
 
-const CSV_URL = "/GuessFootballer/players.csv";
+const CSV_URL = "players.csv";
 
 const PlayerImage = tw.img`
   ${(p) => (p.blurred ? "" : "")}
